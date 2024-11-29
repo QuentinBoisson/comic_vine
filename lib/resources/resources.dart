ef7d67ea9941fac20752eee454156657f7e01bcd
Quentin BOISSON <quentin.boisson@hotmail.fr>
2024-11-29 15:55:33 +0100

    Project Configuration

--- lib/resources/resources.dart
+++
@@ -1,3 +0,0 @@
-part 'app_images.dart';
-
-part 'app_vectorial_images.dart';
--- test/app_images_test.dart
+++ test/app_images_test.dart
@@ -1,7 +1,7 @@
 import 'dart:io';
 
 import 'package:flutter_test/flutter_test.dart';
-import 'package:comic_vine/resources/resources.dart';
+import 'package:comic_vine/assets/resources.dart';
 
 void main() {
   test('app_images assets test', () {});
